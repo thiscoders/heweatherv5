d1d9bb7668058c83c69bfb15db43fa4be99c3f39
ye <1621963477@qq.com>
2016-11-18 19:09:06 +0800

    初始化项目

---
+++ README.md
@@ -0,0 +1,3 @@
+### 使用java对he weather的api进行操作，为android的天气软件I'm 天气做预热
+
+1.第一个测试包
---
+++ src/tes/lgiue/pack01/Exec.java
@@ -0,0 +1,10 @@
+package tes.lgiue.pack01;
+
+/**
+ * Created by ye on 11/18/16.
+ */
+public class Exec {
+    public static void main(String[] args){
+        System.out.println("he weather v5!");
+    }
+}
