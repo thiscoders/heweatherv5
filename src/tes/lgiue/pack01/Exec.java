8d148431153c50d2b59333452137ee63b6ce48ee
ye <1621963477@qq.com>
2016-11-18 19:25:27 +0800

    添加参数

--- src/tes/lgiue/pack01/Exec.java
+++ src/tes/lgiue/pack01/Exec.java
@@ -1,10 +1,30 @@
 package tes.lgiue.pack01;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 /**
  * Created by ye on 11/18/16.
  */
 public class Exec {
-    public static void main(String[] args){
-        System.out.println("he weather v5!");
+    private static String urls;
+    private static String keys;
+
+    static {
+        InputStream inputStream = Exec.class.getClassLoader().getResourceAsStream("mkey.properties");
+
+        Properties properties = new Properties();
+        try {
+            properties.load(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        urls = properties.getProperty("urls");
+        keys = properties.getProperty("keys");
+    }
+
+    public static void main(String[] args) {
+        System.out.println(urls + "\r\n" + keys);
     }
 }
---
+++ src/tes/lgiue/pack01/Weather.java
@@ -0,0 +1,9 @@
+package tes.lgiue.pack01;
+
+/**
+ * Created by ye on 11/18/16.
+ */
+public class Weather {
+
+
+}
